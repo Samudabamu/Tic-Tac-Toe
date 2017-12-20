ce21ab12083f0127b3e90267c17c5af2911a6cee
Samuel Westen <Samuel.westen01@gmail.com>
2017-12-20 15:11:26 +1100

    setup board data structure

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 
+
+// let boardArray = [][];    Why do I need a 2D array
+// boardArray = new int[3][3];
+
+let cells = [ ];  //total cells = column x column
+
+
+
 $(document).ready(function() {
 
 
@@ -6,38 +14,24 @@ $(document).ready(function() {
   let welcome = function (){}  // welcome screen
 
 
-
-
-
-
   let generateBoard = function(num) {
       for( let i = 0; i < num; i++){
-        let $tr = $(`<tr id="${[i]}">`);
+        let $tr = $(`<tr id="tr${[i]}">`); //generate html
+        cells.push([]);     // array construction here
         $('table').append($tr);
+
+
           for( let y = 0; y < num; y++ ){
-            let $th = $('<th>');
-            $(`tr#${i}`).append($th);
+            let $td = $(`<td id="td${[y]}">`);
+            $(`tr#tr${i}`).append($td);
+            cells[i].push(y);
 
         }
     }
   }
+  generateBoard(3);  //Number# of columns
 
 
-  generateBoard(4);
 
 
-
-  $(".cell").click(function(){
-      $('#c1').prepend($('#nought'))
   });
-
-
-
-
-
-
-
-
-
-
-});
