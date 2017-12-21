deebe5374e2895e60ed201978e37a6de7fe86568
Samuel Westen <Samuel.westen01@gmail.com>
2017-12-21 15:08:53 +1100

    created event clicker

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 
 
-// let boardArray = [][];    Why do I need a 2D array
-// boardArray = new int[3][3];
-
 let cells = [ ];  //total cells = column x column
+
+let player1Turn = true;
+
 
 
 
 $(document).ready(function() {
-
 
 
   let welcome = function (){}  // welcome screen
@@ -22,16 +21,53 @@ $(document).ready(function() {
 
 
           for( let y = 0; y < num; y++ ){
-            let $td = $(`<td id="td${[y]}">`);
+            let $td = $(`<td id="td${[i]}${[y]}" data-row=${i} data-column=${y}>`);
             $(`tr#tr${i}`).append($td);
             cells[i].push(y);
-
-        }
+      }
     }
   }
-  generateBoard(3);  //Number# of columns
+  generateBoard(3);  //#No. of columns
 
 
+  $("td").on( "click", function() { //switches between player 1 & 2
+    let $col = $(this).data('column');
+    let $row = $(this).data('row');
 
+
+    if($(this).text() !== "") {
+      return
+    }
+    if (player1Turn === true) {
+      $(this).text("x");
+       cells[$row][$col]='x'
+       console.log($row, $col);
+       winChecker();
+
+    } else {
+      $(this).text("o")
+      cells[$row][$col]='o'
+      console.log($row, $col)
+      winChecker();
+    }
+    player1Turn = !player1Turn;
 
   });
+
+  const winChecker = function(){
+    if (
+    ( cells[0][0] === cells[0][1] && (cells[0][1] === cells[0][2]))
+    // ((cells[1][0] === cells[1][1]) && cells[1][2])) ||
+    // ((cells[2][0] === cells[2][1]) && cells[2][2])) ||
+    // ((cells[0][0] === cells[0][1] && cells[0][2])) ||
+    // ((cells[1][0] === cells[1][1] && cells[1][2])) ||
+    // ((cells[2][0] === cells[2][1] && cells[2][2])) ||
+    // ((cells[0][0] === cells[1][1] && cells[2][2])) ||
+    // ((cells[2][0] === cells[1][1] && cells[0][2])) ||
+    ){
+
+    console.log('somebody wins');
+  }
+}
+
+});
